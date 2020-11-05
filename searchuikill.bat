0442b9aea5d5b2b41949c282473b291b9a6f5b3b
ezfeshar <46002894+ezfeshar@users.noreply.github.com>
2020-11-05 12:34:50 +0330

    Delete searchuikill.bat

--- searchuikill.bat
+++
@@ -1,5 +0,0 @@
-cd %windir%\SystemApps\Microsoft.Windows.Cortana_cw5n1h2txyewy
-takeown /f SearchUI.exe
-icacls SearchUI.exe /grant administrators:F
-taskkill /f /im SearchUI.exe
-rename SearchUI.exe SearchUI.exe.001
